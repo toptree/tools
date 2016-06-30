50c8e201d52efd807662e0fb8247ab48e7c7b426
toptree <toptreegzh@163.com>
2016-06-30 15:17:13 +0800

    提交依赖包

---
+++ src/tools/HttpUtil.java
@@ -0,0 +1,37 @@
+package tools;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+
+
+
+/**
+ * @Description: TODO(用一句话描述该类做什么)
+ * @author gaozhonghui
+ * @mail gaozhh@cdeledu.com
+ * @version V1.0
+ * @date 2016年6月30日
+ */
+
+public class HttpUtil {
+    
+    public static void main(String[] args) {
+	try {
+	   Request.Get("http://toptree.me")
+		      .execute().returnContent();
+	   Request.Post("http://toptree.me")
+	          .bodyForm(Form.form().add("username",  "vip").add("password",  "secret").build())
+	          .execute().returnContent();
+	   Request.Post("http://toptree.me")
+	          .execute().returnContent();
+	} catch (ClientProtocolException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
+	}
+      
+    }
+}
