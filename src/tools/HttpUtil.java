73851327a848894891e3a33948ab8b1b59a210e1
toptree <toptreegzh@163.com>
2016-06-30 15:46:56 +0800

    增加注释

--- src/tools/HttpUtil.java
+++ src/tools/HttpUtil.java
@@ -6,8 +6,6 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.fluent.Form;
 import org.apache.http.client.fluent.Request;
 
-
-
 /**
  * @Description: TODO(用一句话描述该类做什么)
  * @author gaozhonghui
@@ -17,21 +15,27 @@ import org.apache.http.client.fluent.Request;
  */
 
 public class HttpUtil {
-    
+    /**
+     * 
+     * @Method: main
+     * @Description: 测试方法
+     * @param args
+     * @return void
+     */
     public static void main(String[] args) {
 	try {
-	   Request.Get("http://toptree.me")
-		      .execute().returnContent();
-	   Request.Post("http://toptree.me")
-	          .bodyForm(Form.form().add("username",  "vip").add("password",  "secret").build())
-	          .execute().returnContent();
-	   Request.Post("http://toptree.me")
-	          .execute().returnContent();
+	    Request.Get("http://toptree.me").execute().returnContent();
+	    Request.Post("http://toptree.me")
+		    .bodyForm(
+			    Form.form().add("username", "vip")
+				    .add("password", "secret").build())
+		    .execute().returnContent();
+	    Request.Post("http://toptree.me").execute().returnContent();
 	} catch (ClientProtocolException e) {
 	    e.printStackTrace();
 	} catch (IOException e) {
 	    e.printStackTrace();
 	}
-      
+
     }
 }
